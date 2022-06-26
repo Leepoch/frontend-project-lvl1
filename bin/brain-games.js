bbedbdc0e9d37ed406e1c1fd5a84bf7cb2088dc2
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-06-26 11:25:32 +0300

    add brain-even

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 import nameQuestion from '/home/leepoch/frontend-project-lvl1/src/cli.js';
+import parityQuestion from '/home/leepoch/frontend-project-lvl1/bin/brain-even.js'
 
 console.log('Welcome to the Brain Games!');
 nameQuestion();
+console.log(parityQuestion());
