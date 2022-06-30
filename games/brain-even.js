08ff1b38aa4b64ac46768eedda1a7ffb3379d4a7
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-06-30 13:21:04 +0300

    update brain-even.js

--- games/brain-even.js
+++ games/brain-even.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
-import { randomNum } from '/home/leepoch/frontend-project-lvl1/src/utils.js';
-import { generalLogic } from '/home/leepoch/frontend-project-lvl1/src/index.js';
+import randomNum from '/home/leepoch/frontend-project-lvl1/src/utils.js';
+import generalLogic from '/home/leepoch/frontend-project-lvl1/src/index.js';
 
-export const isEven = () => {
-    const arrayOfrightAnswerAndTask = ['', randomNum(20)]
-    if (arrayOfrightAnswerAndTask[1] % 2 === 0) {
-        arrayOfrightAnswerAndTask[0] = 'yes'
-    } else {
-        arrayOfrightAnswerAndTask[0] = 'no'
-    }
-    return arrayOfrightAnswerAndTask
-}
-generalLogic('Answer "yes" if the number is even, otherwise answer "no".')
+const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
+const isEven = () => {
+  const arrayOfrightAnswerAndTask = ['', randomNum(0, 20)];
+  if (arrayOfrightAnswerAndTask[1] % 2 === 0) {
+    arrayOfrightAnswerAndTask[0] = 'yes';
+  } else {
+    arrayOfrightAnswerAndTask[0] = 'no';
+  }
+  return arrayOfrightAnswerAndTask;
+};
+generalLogic(isEven, rules);
