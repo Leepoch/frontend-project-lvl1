ac0c624bf37ff38130cdb8953c5c6e13e0811811
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-06-30 17:50:34 +0300

    fix bug

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-import nameQuestion from '/home/leepoch/frontend-project-lvl1/src/cli.js';
-import parityQuestion from '/home/leepoch/frontend-project-lvl1/bin/brain-even.js'
+import greeting from '/home/leepoch/frontend-project-lvl1/src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-nameQuestion();
-console.log(parityQuestion());
+greeting();
