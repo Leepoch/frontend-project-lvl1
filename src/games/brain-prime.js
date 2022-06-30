e6330e6a4f7ecdb7729c0cae30b0195ff0fedad9
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-06-30 19:05:43 +0300

    brain-prime update

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,17 +3,25 @@ import randomNum from '../utils.js';
 import generalLogic from '../index.js';
 
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const isPrime = (n) => {
+  if (n < 2) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const primeGame = () => {
-  let rightAnswer = 'yes';
-  const num = randomNum(2, 20);
-  if (num % 2 === 0) {
-    rightAnswer = 'no';
-  }
-  if (num === 2) {
-    rightAnswer = 'yes';
-  }
+  const num = randomNum(1, 20);
   const task = num;
-  const arrForReturn = [rightAnswer, task];
-  return arrForReturn;
+  const rightAnswer = (isPrime(num)) ? 'yes' : 'no';
+  return [rightAnswer, task];
 };
 generalLogic(primeGame, rules);
