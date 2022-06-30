954e7b47540af34472371c980a786ed212eefdd6
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-06-30 13:20:32 +0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
-// ОБЩАЯ ЛОГИКА
 import readlineSync from 'readline-sync';
-import { calculater } from '/home/leepoch/frontend-project-lvl1/games/brain-calc.js';
-import { isEven } from '/home/leepoch/frontend-project-lvl1/games/brain-even.js';
 
-const numberOfRounds = 3; 
-export const generalLogic = (rules) => {
-    console.log('Welcome to the Brain Games!')
-    const name = readlineSync.question('May I have your name? ')
-    console.log(`Hello, ${name}!`)
-    console.log(rules)
+const numberOfRounds = 3;
+const generalLogic = (arr, rules) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  console.log(rules);
 
-    for (let i = 0; i < numberOfRounds; i += 1) {
-        const [rightAnswer, task] = calculater()
-        console.log(`Question: ${task}`)
-        const userAnswer = readlineSync.question('Your answer: ');
-        if (userAnswer === String(rightAnswer)) {
-            console.log('Correct!')
-        } else {
-            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`)
-            console.log(`Let's try again, ${name}!`)
-            return
-        }
+  for (let i = 0; i < numberOfRounds; i += 1) {
+    console.log(arr);
+    const [rightAnswer, task] = arr();
+    console.log(`Question: ${task}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === String(rightAnswer)) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
     }
-    console.log(`Congratulations ${name}!`)
-}
-
+  }
+  console.log(`Congratulations ${name}!`);
+};
+export default generalLogic();
