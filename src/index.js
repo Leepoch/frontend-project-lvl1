774744a51fef5d4a89b8e110bf5c6645fa0726a5
Daniil Tinkov <daniiltinkovv@gmail.com>
2022-07-01 17:10:51 +0300

    update general logic

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
 import readlineSync from 'readline-sync';
 
-const numberOfRounds = 3;
-const generalLogic = (game, rules) => {
+const numbersCount = 3;
+const generalLogic = (getRoundDate, rule) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-  console.log(rules);
+  console.log(rule);
 
-  for (let i = 0; i < numberOfRounds; i += 1) {
-    const [rightAnswer, task] = game();
+  for (let i = 0; i < numbersCount; i += 1) {
+    const [rightAnswer, task] = getRoundDate();
     console.log(`Question: ${task}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === String(rightAnswer)) {
-      console.log('Correct!');
-    } else {
+    if (userAnswer !== rightAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${name}!`);
 };
